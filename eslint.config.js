17dab23bcc63a91f62b27cea79b145d6481353df
Lindsay <51120285+loslds@users.noreply.github.com>
2025-01-17 10:28:18 -0300

    01

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,10 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  //{ ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -16,6 +17,10 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react,
+    },
+    settings: {
+      react: { version: '18.3' },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +28,48 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
     },
   },
 )
+
+
+
+
+
+
+
+
+
+
+
+
+//import js from '@eslint/js'
+//import globals from 'globals'
+//import reactHooks from 'eslint-plugin-react-hooks'
+//import reactRefresh from 'eslint-plugin-react-refresh'
+//import tseslint from 'typescript-eslint'
+
+//export default tseslint.config(
+//  { ignores: ['dist'] },
+//  {
+//    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+//    files: ['**/*.{ts,tsx}'],
+//    languageOptions: {
+//      ecmaVersion: 2020,
+//      globals: globals.browser,
+//    },
+//    plugins: {
+//      'react-hooks': reactHooks,
+//      'react-refresh': reactRefresh,
+//    },
+//    rules: {
+//      ...reactHooks.configs.recommended.rules,
+//      'react-refresh/only-export-components': [
+//        'warn',
+//        { allowConstantExport: true },
+//      ],
+//    },
+//  },
+//)
